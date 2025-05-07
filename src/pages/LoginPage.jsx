a53101d06b40f979426c6c5c4582215395895b06
Coconut <coconutistaster@gmail.com>
2025-05-07 11:08:24 +0700

    Updated! Ver 4.2.1
    + Update the Login page for much more convenience and more user-friendly for the css.
    
    + Also implemented the .module.css for the specific usage for each pages (right now is for the loginPage.jsx)

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import style from './LoginPage.module.css'
 
-
 function LoginPage() {
+
    return (
-      <>
-         <div className={style.mainLoginContainer}>
-            <p id={style.loginTitle}>Login Page</p>
-            <div className={style.formContainer}>
-               <form>
-                  <label>
-                     Username:
-                     <input type="text" name="username" id={style.input_username} />
-                  </label>
-                  <br />
-                  <label>
-                     Password:
-                     <input type="password" name="password" id={style.input_password} />
-                  </label>
-                  <br />
-                  <button type="submit">Login</button>
-                  <button type="button">Forgot Password</button>
-                  <button type="button">Cancel</button>
-               </form>
-            </div>
+      <div className={style.mainLoginContainer}>
+         <p className={style.loginTitle}>Login Page</p>
+
+         <div className={style.formContainer}>
+            <form className={style.loginForm}>
+               <div className={style.inputGroup}>
+                  <label htmlFor="username">Username:</label>
+                  <input type="text" id="username" name="username" />
+               </div>
+
+               <div className={style.inputGroup}>
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" name="password" />
+               </div>
+
+               <div className={style.buttonGroup}>
+                  <button type="submit" className={style.loginButton}>Login</button>
+                  <button type="button"
+                     className={style.forgotButton} 
+                     onClick={() => 
+                     alert("Please contact Admin Via universalconversion.admin@gmail.com")}>
+                        Forgot Password
+                  </button>
+                  <p>Don't have an account? <br /> Register!</p>
+                  <button type="button" className={style.registerButton}>Register</button>
+                  <button type="button" className={style.cancelButton}>Cancel</button>
+               </div>
+            </form>
          </div>
-      </>
+      </div>
    )
 }
 
